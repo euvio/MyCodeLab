eebaf56fd5ad5ee637596a58006b96de45f7ce73
euv <euvhere@outlook.com>
2024-01-08 17:23:33 +0800

    f

---
+++ MyCodeLab/CommonLibrary/FakeModel.cs
@@ -0,0 +1,15 @@
+﻿using Prism.Mvvm;
+
+namespace CommonLibrary
+{
+    public class FakeModel : BindableBase
+    {
+        private string name;
+        private int age;
+        private string email;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public string Email { get => email; set => email = value; }
+    }
+}
---
+++ MyCodeLab/DataGridAbout/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommonLibrary;
+using Prism.Commands;
+
+namespace DataGridAbout
+{
+    internal class MainViewModel
+    {
+        public DelegateCommand ClickCommand { get; set; } = new DelegateCommand(() =>
+        {
+            ;
+        });
+
+        public List<FakeModel> FakeModels { get; set; } = new List<FakeModel>()
+        {
+            new FakeModel() {
+                        Age = 1,
+                        Name = "Foo",
+                        Email = "203232@qq.com"
+        },
+
+                     new FakeModel() {
+                     Age = 2,
+                     Name = "CC",
+                     Email = "uids@outlook.com"
+                    }
+        };
+    }
+}
