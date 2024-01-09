a2d7ef6a9bea718c466e5f72a3026bbe3e0c7943
euv <euvhere@outlook.com>
2024-01-10 01:06:25 +0800

    .

--- MyCodeLab/CommonLibrary/FakeModel.cs
+++ MyCodeLab/CommonLibrary/FakeModel.cs
@@ -7,9 +7,11 @@ namespace CommonLibrary
         private string name;
         private int age;
         private string email;
+        private double salary;
 
         public string Name { get => name; set => name = value; }
         public int Age { get => age; set => age = value; }
         public string Email { get => email; set => email = value; }
+        public double Salary { get => salary; set => salary = value; }
     }
 }
